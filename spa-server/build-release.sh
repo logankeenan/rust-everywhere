88d603c5373e0b62e9ea999755339bdd53d06159
Logan Keenan <d.logankeenan@gmail.com>
2023-07-25 21:42:15 -0500

    update the build release for spa-server

--- spa-server/build-release.sh
+++ spa-server/build-release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ../spa
+cargo clean
 . ./scripts/release.sh
 
 cp -r dist ../spa-server
@@ -9,5 +10,5 @@ cd ../spa-server
 mkdir -p dist/axum-browser-adapter/
 cp node_modules/axum-browser-adapter/index.js dist/axum-browser-adapter/index.js
 
-#cargo install cross
-#cross build --target aarch64-unknown-linux-gnu --release
+cargo install cross
+cross build --target aarch64-unknown-linux-gnu --release
