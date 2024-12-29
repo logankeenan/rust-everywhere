1e20cf287cce12832eae1d3cab13f22aa4cb8cf9
Logan Keenan <d.logankeenan@gmail.com>
2024-12-29 11:20:25 -0600

    update build-release.sh for render.com

--- api/build-release.sh
+++ api/build-release.sh
@@ -3,10 +3,4 @@
 . ./sqlx.sh database create
 . ./sqlx.sh migrate run
 
-# raspberry pi
-#cargo install cross
-#cross build --target aarch64-unknown-linux-gnu --release
-
-# Digital Ocean
-cargo install cross
-cross build --target x86_64-unknown-linux-gnu --release
+cargo build --release
